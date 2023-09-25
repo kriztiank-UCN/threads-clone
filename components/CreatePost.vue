7f1f1014b1967992f2ca066afbc2359a5d7f5da0
Kristian <10221425@ucn.dk>
2023-09-25 13:13:25 +0200

    Create Post Supabase Function | File Upload |

--- components/CreatePost.vue
+++ components/CreatePost.vue
@@ -15,8 +15,13 @@
         <div class="py-2 w-full">
           <div class="flex items-center">
             <div v-if="user" class="flex items-center text-white">
-              <img class="rounded-full h-[35px]" :src="user.identities[0].identity_data.avatar_url" />
-              <div class="ml-2 font-semibold text-[18px]">{{ user.identities[0].identity_data.full_name }}</div>
+              <img
+                class="rounded-full h-[35px]"
+                :src="user.identities[0].identity_data.avatar_url"
+              />
+              <div class="ml-2 font-semibold text-[18px]">
+                {{ user.identities[0].identity_data.full_name }}
+              </div>
             </div>
           </div>
           <!-- DYNAMIC TEXTAREA -->
@@ -35,10 +40,10 @@
                   class="w-full bg-black outline-none"
                 ></textarea>
               </div>
-              <!-- SO FAR... -->
+              <!-- FILE UPLOAD... -->
               <div class="w-full">
                 <div class="flex flex-col gap-2 py-1">
-                  <!-- if file display has something in it we want to display this image and we've got a source which is file display and then finally underneath that div we have a little label that is going to be our file input -->
+                  <!-- if fileDisplay has something in it we want to display this image and we've got a source which is fileDisplay and then underneath that div we have a label that is going to be our file input -->
                   <div v-if="fileDisplay">
                     <img class="mx-auto w-full mt-2 mr-2 rounded-lg" :src="fileDisplay" />
                   </div>
@@ -112,5 +117,56 @@ const clearData = () => {
 const onChange = () => {
   fileDisplay.value = URL.createObjectURL(file.value.files[0])
   fileData.value = file.value.files[0]
+}
+
+// CREATE POST
+const createPost = async () => {
+  let dataOut = null
+  let errorOut = null
+
+  isLoading.value = true
+  // if we have a file we want to upload it to supabase storage
+  if (fileData.value) {
+    const { data, error } = await client.storage
+      .from('threads-clone-files')
+      .upload(`${uuidv4()}.jpg`, fileData.value)
+
+    dataOut = data
+    errorOut = error
+  }
+  // if there is an error we stop the process and return the error
+  if (errorOut) {
+    console.log(errorOut)
+    return errorOut
+  }
+  // if there is not an error, set pic to nothing because we can create the post without an image
+  // if there is a path we're going to assign that to the picture
+  let pic = ''
+  if (dataOut) {
+    pic = dataOut.path ? dataOut.path : ''
+  }
+
+  try {
+    await useFetch(`/api/create-post/`, {
+      method: 'POST',
+      body: {
+        userId: user.value.identities[0].user_id,
+        name: user.value.identities[0].identity_data.full_name,
+        image: user.value.identities[0].identity_data.avatar_url,
+        text: text.value,
+        picture: pic,
+      },
+    })
+    // get fresh data from the database
+    await userStore.getAllPosts()
+    // close the menu overlay
+    userStore.isMenuOverlay = false
+
+    clearData()
+    isLoading.value = false
+  } catch (error) {
+    console.log(error)
+    isLoading.value = false
+  }
 }
 </script>
